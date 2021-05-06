9ac8e4af665107fbcc1ba4ca7f95026b140a9aa3
lucimarlidia <lucimarlidia@gmail.com>
2021-05-06 14:16:03 -0300

    Treinamento_Lucimar_2

--- TreinamentoWeb.Services/Services/ProdutoService.cs
+++ TreinamentoWeb.Services/Services/ProdutoService.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TreinamentoWeb.Core.Entities;
+using TreinamentoWeb.Core.Interfaces;
 
 namespace TreinamentoWeb.Services.Services
 {
-    class ProdutoService
+    public class ProdutoService :  BaseService<Product>
     {
+        public ProdutoService(IRepository<Product> repository) : base(repository)
+        {
+        }
+
+        protected override void ValidateEntity(Product entity)
+        {
+            var hasDescription = string.IsNullOrWhiteSpace(entity.Description);
+            var hasKind = string.IsNullOrWhiteSpace(entity.Kind);
+            var hasManufacturer = string.IsNullOrWhiteSpace(entity.Manufacturer);
+            var hasPrice = entity.Price > 0;
+
+            if (hasDescription || hasKind || hasManufacturer || hasPrice)
+                throw new ArgumentException("Produto inválido");
+        }
     }
 }
