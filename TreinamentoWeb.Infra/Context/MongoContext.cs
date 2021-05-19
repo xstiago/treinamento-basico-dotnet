50231fe01927ce2c1c0e9f617820bf2e37eb94bc
Tiago Xavier da Silva <tiago.xavier@ambevtech.com.br>
2021-05-19 16:11:05 -0300

    Added mongo classes

---
+++ TreinamentoWeb.Infra/Context/MongoContext.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TreinamentoWeb.Infra.Context
+{
+    public class MongoContext : IMongoContext
+    {
+        private readonly ICollection<Func<Task>> _commands;
+        private readonly string _mongoConncetionString;
+        private IMongoClient mongoClient;
+        private IMongoDatabase mongoDatabase;
+        private IClientSessionHandle sessionHandle;
+
+        private void ConfigureMongo()
+        {
+            if (mongoClient != null)
+            {
+                return;
+            }
+
+            mongoClient = new MongoClient(_mongoConncetionString);
+
+            var dbName = MongoUrl.Create(_mongoConncetionString).DatabaseName;
+            mongoDatabase = mongoClient.GetDatabase(dbName);
+        }
+
+        public MongoContext(IConfiguration configuration)
+        {
+            _mongoConncetionString = configuration.GetSection("MongoDb:ConnectionString").Value;
+            _commands = new List<Func<Task>>();
+        }
+
+        public void AddCommand(Func<Task> func)
+        {
+            _commands.Add(func);
+        }
+
+        public void Dispose()
+        {
+            sessionHandle?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public IMongoCollection<T> GetCollection<T>(string name)
+        {
+            ConfigureMongo();
+
+            return mongoDatabase.GetCollection<T>(name);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            if (!_commands.Any())
+            {
+                return 0;
+            }
+
+            ConfigureMongo();
+
+            using (sessionHandle = await mongoClient.StartSessionAsync())
+            {
+                var commandTasks = _commands.Select(c => c());
+
+                await Task.WhenAll(commandTasks);
+            }
+
+            var affectedRows = _commands.Count;
+            
+            _commands.Clear();
+
+            return affectedRows;
+        }
+    }
+}
---
+++ TreinamentoWeb.Infra/Context/UnitOfWork.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+
+namespace TreinamentoWeb.Infra.Context
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly IMongoContext _context;
+
+        public UnitOfWork(IMongoContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<bool> CommitAsync()
+        {
+            var changeAmount = await _context.SaveChangesAsync();
+            return changeAmount > 0;
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+    }
+}
