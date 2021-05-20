b6c50335aa32bf3ff72dce0f73221c94ad8cf8c0
Jorge Patrick <jorgepatrickdev@gmail.com>
2021-05-20 16:01:02 -0300

    DocumentCustomerHelperTests

---
+++ TreinamentoWeb.Unit.Tests/Services/Helpers/DocumentCustomerHelperTests.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TreinamentoWeb.Services.Helpers;
+using Xunit;
+
+namespace TreinamentoWeb.Unit.Tests.Services.Helpers
+{
+    public class DocumentCustomerHelperTests
+    {
+        [Fact]
+        public async Task ShouldReturnTrueWhenCpfIsValid()
+        {
+            #region Arrange
+
+            const string cpf = "54822590054";
+
+            #endregion Arrange
+
+            #region Act
+
+            var sucess = DocumentCustomerHelper.ValidateCpf(cpf);
+
+            #endregion Act
+
+            #region Assert
+
+            Assert.True(sucess);
+
+            #endregion Assert
+        }
+    }
+}
