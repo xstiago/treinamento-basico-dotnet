c0c597cf3e43eb351f643869aa01e0354d616bd9
lucimarlidia <lucimarlidia@gmail.com>
2021-05-20 15:39:20 -0300

    Test

--- TreinamentoWeb.Unit.Tests/Services/NatualPersonServiceTests.cs
+++ TreinamentoWeb.Unit.Tests/Services/NatualPersonServiceTests.cs
@@ -34,6 +34,8 @@ namespace TreinamentoWeb.Unit.Tests.Services
             var validator = new NaturalPersonValidator();
 
             var service = new NaturalPersonService(repositoryMock.Object, validator);
+                      
+
 
             #endregion Arrange
 
@@ -47,6 +49,45 @@ namespace TreinamentoWeb.Unit.Tests.Services
 
             Assert.Equal(expectedChangedRecords, changedRecords);
             Assert.Null(errors);
+
+            #endregion Assert
+        }
+
+        [Fact]
+        public async Task ShouldNotSaveDataWhenValidationDoesntWork()
+        {
+            #region Arrange
+
+            const int expectedChangedRecords = -1;
+
+            var payload = new NaturalPerson
+            {
+                Active = true,
+                Address = "Rua XXX",
+                CPF = "54822590053",
+                Email = "fulano@terra.com.br",
+                Name = "Fulano da Silva"
+            };
+
+            var repositoryMock = new Mock<IRepository<NaturalPerson>>();
+
+            var validator = new NaturalPersonValidator();
+
+            var service = new NaturalPersonService(repositoryMock.Object, validator);
+
+            #endregion Arrange
+
+            #region Act
+
+            (int changedRecords, IEnumerable<string> errors) = await service.Save(payload);
+
+            #endregion Act
+
+            #region Assert
+
+            Assert.Equal(expectedChangedRecords, changedRecords);
+            Assert.NotNull(errors);
+            Assert.NotEmpty(errors);
 
             #endregion Assert
         }
