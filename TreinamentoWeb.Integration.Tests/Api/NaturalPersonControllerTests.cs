d431fb253277fa65b923e1957c4e5c5eacf50aef
Tiago Xavier da Silva <tiago.xavier@ambevtech.com.br>
2021-06-01 15:57:04 -0300

    Added integration tests

---
+++ TreinamentoWeb.Integration.Tests/Api/NaturalPersonControllerTests.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+using TreinamentoWeb.Api;
+using TreinamentoWeb.Core.Entities;
+using TreinamentoWeb.Integration.Tests.Fixture;
+using Xunit;
+
+namespace TreinamentoWeb.Integration.Tests.Api
+{
+    public class NaturalPersonControllerTests : IClassFixture<WebApplicationFactory<Startup>>, IDisposable
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+        private readonly MongoDbFixture _dbFixture;
+
+        public NaturalPersonControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+            _dbFixture = new MongoDbFixture();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing) 
+        {
+            if (disposing)
+            {
+                _dbFixture?.Dispose();
+            }
+        }
+
+        [Fact]
+        public async Task Should_Save_Natural_Person_On_Repository_When_Receive_Valid_Data()
+        {
+            #region Arrange
+
+            var client = _factory.CreateClient();
+
+            var payload = new NaturalPerson
+            {
+                Active = true,
+                Address = "Rua XXX",
+                CPF = "54822590054",
+                Email = "fulano@terra.com.br",
+                Name = "Fulano da Silva"
+            };
+
+            #endregion Arrange
+
+            #region Act
+
+            var responseMessage = await client.PostAsJsonAsync("api/NaturalPerson/", payload);
+
+            #endregion Act
+
+            #region Assert
+
+            responseMessage.EnsureSuccessStatusCode();
+
+            var result = await responseMessage.Content.ReadAsStringAsync();
+
+            Assert.Equal("Cliente inserido com sucesso!", result);
+
+            #endregion Assert
+        }
+
+        [Fact]
+        public async Task Should_Get_Natural_Person_From_Repository_When_There_Are_Data()
+        {
+            #region Arrange
+
+            var client = _factory.CreateClient();
+
+            var payload = new NaturalPerson
+            {
+                Active = true,
+                Address = "Rua XXX",
+                CPF = "77522810000",
+                Email = "fulano@terra.com.br",
+                Name = "Fulano da Silva"
+            };
+
+            await _dbFixture.AddSync(payload);
+
+            #endregion Arrange
+
+            #region Act
+
+            var responseMessage = await client.GetAsync("api/NaturalPerson/");
+
+            #endregion Act
+
+            #region Assert
+
+            responseMessage.EnsureSuccessStatusCode();
+
+            var jsonResult = await responseMessage.Content.ReadAsStringAsync();
+
+            var naturalPersonList = JsonSerializer.Deserialize<IEnumerable<NaturalPerson>>(jsonResult, new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            });
+
+            Assert.NotEmpty(naturalPersonList);
+            Assert.Contains(naturalPersonList, o => o.CPF == payload.CPF);
+
+            #endregion Assert
+        }
+    }
+}
---
+++ TreinamentoWeb.Integration.Tests/Fixture/MongoDbFixture.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+using TreinamentoWeb.Core.Entities;
+using TreinamentoWeb.Infra.Context;
+using TreinamentoWeb.Infra.Repositories;
+
+namespace TreinamentoWeb.Integration.Tests.Fixture
+{
+    public class MongoDbFixture : IDisposable
+    {
+        private readonly string _connectionString;
+
+        public MongoContext DbContext { get; private set; }
+
+        public MongoDbFixture()
+        {
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            _connectionString = config["MongoDb:ConnectionString"];
+
+            DbContext = new MongoContext(config);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                var client = new MongoClient(_connectionString);
+                var dbName = MongoUrl.Create(_connectionString).DatabaseName;
+                client.DropDatabase(dbName);
+
+                DbContext.Dispose();
+            }
+        }
+
+        public async Task AddSync(NaturalPerson entity)
+        {
+            var repository = new NaturalPersonRepository(DbContext);
+
+            await repository.Save(entity);
+        }
+    }
+}
